b9c880389ff9fa5dad174015ec95233691147ec3
Duc Anh <clok0001@gmail.com>
2025-10-05 21:09:09 +0700

    add home servlet

--- src/main/java/com/mycompany/swp391/controller/home/HomeServlet.java
+++ src/main/java/com/mycompany/swp391/controller/home/HomeServlet.java
@@ -23,7 +23,7 @@ public class HomeServlet extends HttpServlet {
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
      *
-     * @param request servlet request
+     * @param request servlet requestt
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
