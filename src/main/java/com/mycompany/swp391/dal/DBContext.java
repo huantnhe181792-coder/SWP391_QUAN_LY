df63c04534d63284754c49c5e50deeac9b097974
thaidqhe181510-jpg <thaidqhe181510@fpt.edu.vn>
2025-10-18 11:28:46 +0700

    ClubServlet
    controller mana

--- src/main/java/com/mycompany/swp391/dal/DBContext.java
+++ src/main/java/com/mycompany/swp391/dal/DBContext.java
@@ -4,7 +4,6 @@
  */
 package com.mycompany.swp391.dal;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -15,44 +14,44 @@ import java.util.logging.Logger;
 
 public class DBContext {
 
-  public Connection connection;
-  protected ResultSet resultSet;
-  public PreparedStatement statement;
+    public Connection connection;
+    protected ResultSet resultSet;
+    public PreparedStatement statement;
 
-  public DBContext() {
-    try {
-      // Change the username password and url to connect your own database
-      String username = "root";
-      String password = "123456aA@";
-      String url = "jdbc:mysql://localhost:3306/swp391_quan_ly";
-      Class.forName("com.mysql.cj.jdbc.Driver");
-      connection = DriverManager.getConnection(url, username, password);
-    } catch (ClassNotFoundException | SQLException ex) {
-      Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+    public DBContext() {
+        try {
+            // Change the username password and url to connect your own database
+            String username = "root";
+            String password = "root";
+            String url = "jdbc:mysql://localhost:3306/swp391_quan_ly";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-  }
 
-  public void closeResources() {
-    try {
-      if (resultSet != null && !resultSet.isClosed()) {
-        resultSet.close();
-      }
-      if (statement != null && !statement.isClosed()) {
-        statement.close();
-      }
-      if (connection != null && !connection.isClosed()) {
-        connection.close();
-      }
-    } catch (SQLException ex) {
-      Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+    public void closeResources() {
+        try {
+            if (resultSet != null && !resultSet.isClosed()) {
+                resultSet.close();
+            }
+            if (statement != null && !statement.isClosed()) {
+                statement.close();
+            }
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-  }
 
-  public Connection getConnection() {
-    return new DBContext().connection;
-  }
+    public Connection getConnection() {
+        return new DBContext().connection;
+    }
 
-  public static void main(String[] args) {
-    System.out.println(new DBContext().connection);
-  }
+    public static void main(String[] args) {
+        System.out.println(new DBContext().connection);
+    }
 }
--- src/main/java/com/mycompany/swp391/utils/EmailUtils.java
+++ src/main/java/com/mycompany/swp391/utils/EmailUtils.java
@@ -1,6 +1,7 @@
 package com.mycompany.swp391.utils;
 
 import com.mycompany.swp391.config.GlobalConfig;
+import com.mycompany.swp391.utils.GlobalUtils;
 import jakarta.mail.Message;
 import jakarta.mail.MessagingException;
 import jakarta.mail.PasswordAuthentication;
@@ -23,71 +24,89 @@ import java.util.logging.Logger;
  */
 public class EmailUtils {
 
-    public static boolean sendMail(String to, String subject, String content)
-            throws AddressException, MessagingException {
-        Properties props = new Properties(); // Properties là một lớp có sẵn trong Java dùng để lưu trữ các cặp khóa – giá
-        // trị, trong đó cả key và value đều là kiểu String.
-        // Thiết lập các thuộc tính cho phiên gửi mail
-        props.put("mail.smtp.host", "smtp.gmail.com"); // SMTP server của Gmail
-        props.put("mail.smtp.port", "587"); // Cổng TLS của Gmail TLS (Transport Layer Security) là một giao thức bảo mật
-        // được thiết kế để mã hóa dữ liệu khi truyền qua Internet
-        props.put("mail.smtp.auth", "true"); // Xác thực email để gửi
-        props.put("mail.smtp.starttls.enable", "true"); // Bật STARTTLS để bảo mật
+  public static boolean sendMail(String to, String subject, String content)
+      throws AddressException, MessagingException {
+    Properties props = new Properties(); // Properties là một lớp có sẵn trong Java dùng để lưu trữ các cặp khóa – giá
+    // trị, trong đó cả key và value đều là kiểu String.
+    // Thiết lập các thuộc tính cho phiên gửi mail
+    props.put("mail.smtp.host", "smtp.gmail.com"); // SMTP server của Gmail
+    props.put("mail.smtp.port", "587"); // Cổng TLS của Gmail TLS (Transport Layer Security) là một giao thức bảo mật
+    // được thiết kế để mã hóa dữ liệu khi truyền qua Internet
+    props.put("mail.smtp.auth", "true"); // Xác thực email để gửi
+    props.put("mail.smtp.starttls.enable", "true"); // Bật STARTTLS để bảo mật
 
-        Session session = Session.getInstance(props, new jakarta.mail.Authenticator() { // Session.getInstance đại diện cho
-            // 1 phiên gửi email và chứa các
-            // thông tin từ props và new
-            // jakarta.mail.Authenticator() là
-            // để ghi đè cho phương thức
-            // getPasswordAuthentication()
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() { // Phương thức được gọi tự động khi cần xác thực
-                // với Gmail.
-                return new PasswordAuthentication(GlobalConfig.USERNAME_EMAIL, GlobalConfig.PASSWORD_APP_EMAIL);
-            }
-        });
+    Session session = Session.getInstance(props, new jakarta.mail.Authenticator() { // Session.getInstance đại diện cho
+      // 1 phiên gửi email và chứa các
+      // thông tin từ props và new
+      // jakarta.mail.Authenticator() là
+      // để ghi đè cho phương thức
+      // getPasswordAuthentication()
+      @Override
+      protected PasswordAuthentication getPasswordAuthentication() { // Phương thức được gọi tự động khi cần xác thực
+        // với Gmail.
+        return new PasswordAuthentication(GlobalConfig.USERNAME_EMAIL, GlobalConfig.PASSWORD_APP_EMAIL);
+      }
+    });
 
-        MimeMessage message = new MimeMessage(session); // Tạo ra một email mới (đối tượng MimeMessage) dựa trên Session đã
-        // cấu hình trước đó.
-        message.setFrom(new InternetAddress(GlobalConfig.USERNAME_EMAIL)); // Thiết lập địa chỉ người gửi (From).
-        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to)); // Thiết lập người nhận (To).
-        message.setSubject(subject, "UTF-8"); // Thiết lập tiêu đề (subject) của email
-        message.setHeader("Content-Type", "text/html; charset=UTF-8"); // Thiết lập header cho email
-        message.setContent(content, "text/html; charset=UTF-8"); // Đặt nội dung chính cho email
+    MimeMessage message = new MimeMessage(session); // Tạo ra một email mới (đối tượng MimeMessage) dựa trên Session đã
+    // cấu hình trước đó.
+    message.setFrom(new InternetAddress(GlobalConfig.USERNAME_EMAIL)); // Thiết lập địa chỉ người gửi (From).
+    message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to)); // Thiết lập người nhận (To).
+    message.setSubject(subject, "UTF-8"); // Thiết lập tiêu đề (subject) của email
+    message.setHeader("Content-Type", "text/html; charset=UTF-8"); // Thiết lập header cho email
+    message.setContent(content, "text/html; charset=UTF-8"); // Đặt nội dung chính cho email
 
-        Transport.send(message); // Gửi email đi qua SMTP server. Dùng Session đã cấu hình và Authenticator để
-        // đăng nhập và gửi
-        return true;
+    Transport.send(message); // Gửi email đi qua SMTP server. Dùng Session đã cấu hình và Authenticator để
+    // đăng nhập và gửi
+    return true;
+  }
+
+  public static String sendOTPMail(String to) {
+    int otp = GlobalUtils.generateOTP(6); // Sử dụng phương thức generateOTP để tạo OTP
+    String subject = "Mã OTP";
+    String content = "Mã OTP của bạn là: " + otp + "<br><br>Mã OTP sẽ hết hạn sau 5 phút.";
+
+    try {
+      sendMail(to, subject, content);
+    } catch (MessagingException ex) {
+      Logger.getLogger(EmailUtils.class.getName()).log(Level.SEVERE, null, ex);
     }
 
-    public static String sendOTPMail(String to) {
-        int otp = GlobalUtils.generateOTP(6); // Sử dụng phương thức generateOTP để tạo OTP
-        String subject = "Mã OTP";
-        String content = "Mã OTP của bạn là: " + otp + "<br><br>Mã OTP sẽ hết hạn sau 5 phút.";
+    return otp + "";
+  }
 
-        try {
-            sendMail(to, subject, content);
-        } catch (MessagingException ex) {
-            Logger.getLogger(EmailUtils.class.getName()).log(Level.SEVERE, null, ex);
-        }
+  public static String sendAccountMail(String to, String email, String password) throws MessagingException {
+    String subject = "Tài Khoản Truy Cập Hệ Thống CLubs FPT University:";
+    String content = "<h3>Xin chào,</h3>"
+        + "<p>Bạn đã được cấp một tài khoản để truy cập hệ thống:</p>"
+        + "<p><strong>Tên đăng nhập:</strong> " + email + "</p>"
+        + "<p><strong>Mật khẩu:</strong> " + password + "</p>"
+        + "<p>Hãy đăng nhập và thay đổi mật khẩu ngay sau lần đăng nhập đầu tiên.</p>"
+        + "<br><p>Trân trọng!</p>";
 
-        return otp + "";
+    boolean sent = sendMail(to, subject, content);
+    if (sent) {
+      return "Gửi tài khoản qua email thành công!";
+    } else {
+      return "Gửi email thất bại!";
+    }
+  }
+
+  public static String sendStatusForClub(String email, String status, String reason) throws MessagingException {
+    String subject = "Thông báo cập nhật trạng thái câu lạc bộ của bạn:";
+    String content = "<h3>Xin chào,</h3>";
+    if (status.equals("active")) {
+      content += "<p>Câu lạc bộ của bạn đã hoạt động trở lại!</p>";
+    } else {
+      content += "<p>Câu lạc bộ của bạn không được phép hoạt động trong thời gian tới !</p>";
+      content += "Lí do: " + reason;
     }
 
-    public static String sendAccountMail(String to, String email, String password) throws MessagingException {
-        String subject = "Tài Khoản Truy Cập Hệ Thống CLubs FPT University:";
-        String content = "<h3>Xin chào,</h3>"
-                + "<p>Bạn đã được cấp một tài khoản để truy cập hệ thống:</p>"
-                + "<p><strong>Tên đăng nhập:</strong> " + email + "</p>"
-                + "<p><strong>Mật khẩu:</strong> " + password + "</p>"
-                + "<p>Hãy đăng nhập và thay đổi mật khẩu ngay sau lần đăng nhập đầu tiên.</p>"
-                + "<br><p>Trân trọng!</p>";
-
-        boolean sent = sendMail(to, subject, content);
-        if (sent) {
-            return "Gửi tài khoản qua email thành công!";
-        } else {
-            return "Gửi email thất bại!";
-        }
+    boolean sent = sendMail(email, subject, content);
+    if (sent) {
+      return "Gửi thông báo trạng thái câu lạc bộ qua email thành công!";
+    } else {
+      return "Gửi email thất bại!";
     }
+  }
 }
